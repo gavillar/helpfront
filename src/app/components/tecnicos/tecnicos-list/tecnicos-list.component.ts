d5d7cbd7b3626014052d37c276801a55d327c65d
gavillar <gabriel_gomesvillar@hotmail.com>
2023-04-23 15:39:45 -0300

    Create Table Tecnicos

--- src/app/components/tecnicos/tecnicos-list/tecnicos-list.component.ts
+++ src/app/components/tecnicos/tecnicos-list/tecnicos-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnicos';
+
+
 
 @Component({
   selector: 'app-tecnicos-list',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicosListComponent implements OnInit {
 
+  
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Gabriel',
+      cpf: '37666549857',
+      email: 'gabriel@mail.com',
+      senha: '123',
+      perfis: ['0'],
+      dataCriacao: '15/08/2022'
+    }
+  ]
+
+  displayedColumns: string[] = ['Id', 'Nome', 'CPF', 'Email', 'Açoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+  
+  
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
+
